4a782129a1e98b6e8625272f6f3991c563464c8b
Asuka Minato <i@asukaminato.eu.org>
2025-02-14 01:49:58 +0900

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,34 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "unstable"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.aria2
+    pkgs.croc
+    pkgs.fish
+    pkgs.qbittorrent-nox
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        # Open editors for the following files by default, if they exist:
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+      };
+    };
+  };
+}
